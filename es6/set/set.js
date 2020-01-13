7be07952442ef77ee622ce01b7bc44154fa77211
duanxinxin <duanxinxins@163.com>
2020-01-13 17:41:30 +0800

    set

--- es6/set/set.js
+++ es6/set/set.js
@@ -1,4 +1,4 @@
-// set 类似于数组，成员值是唯一的
+// set 类似于数组，成员值是唯一的，
 // set 可使用构造函数构造,接受一个数组或者具有 (iterable 接口的其他数据结构,例如：document.querySelectorAll('div') arguments）作为参数，用来初始化。
 //  set 可用于数组去重，但是对于 对象,数组，方法 无法去重
 let foo = Symbol('foo')
@@ -13,8 +13,11 @@ console.log(setOne.has(1))
 console.log(setOne.delete(1))
 // console.log(setOne.clear())
 console.log('keys->',setOne.keys())
+for(let item of setOne.keys()){
+  console.log('keys item->',item)
+}
 console.log('value->',setOne.values())
 console.log('entries->',setOne.entries())
 setOne.forEach((item,key) => {
-  console.log('setOne->',key,item)
+  console.log('setOne value->',key,item)
 })
