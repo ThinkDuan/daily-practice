8eb236f3a03abcf75806776bdf207f51e89d72a1
段鑫鑫 <duanxinxin@msyizu.com>
2022-03-08 18:08:26 +0800

    save

--- es6/let/let.js
+++ es6/let/let.js
@@ -1,12 +1,42 @@
 function foo(){
   console.log('I am outside')
 }
-(function(){
+(function(foo){
   if(false){
    function foo(){
       console.log('I am inside')
     }
   }
-  console.log(foo)
-  foo()
-})()
+  console.log('IEEF内部', foo)
+  if (typeof foo === 'function') {
+    foo()
+  }
+})(foo)
+const a = ''
+// console.log(b)
+let b = 'name'
+
+for (let i = 0;i<5;i++) {
+  console.log(i)
+}
+console.log(i)
+
+for (var i = 0;i<5;i++) {
+  console.log(i)
+}
+console.log(i)
+
+console.log(a)
+let a = 'name';
+var b = '1';
+const c = 'const';
+for (let i = 0;i<5;i++) {
+  debugger
+  console.log(i)
+}
+{
+    // var a;
+    console.log(b)
+    let b;
+}
+console.log(i)
